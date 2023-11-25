78e1b5794ce71157c613635cbe01b123bc5868d3
v4sh <tioban.alm@gmail.com>
2023-11-25 16:09:54 -0300

    instalacao do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
+const exphbs = require(ëxpress-handlebars)
+
+app.engine("handlebars", exphbs.engine)
+app.set("view engine", "handlebars")
+
 app.get('/', (requisicao, resposta) => {
     resposta.send("ola mundo")
 })
