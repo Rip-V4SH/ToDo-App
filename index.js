915a72c47e24c594ceba80cc52ecc10716529445
v4sh <tioban.alm@gmail.com>
2023-11-29 21:05:23 -0300

    iniciando o filtro de tarefas ativas

--- index.js
+++ index.js
@@ -67,7 +67,9 @@ app.post('/criar', (requisicao, resposta) => {
     })
 })
 
-
+app.get('/ativas', (requisicao, resposta) => {
+    
+})
 
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
